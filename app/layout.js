2b4809d868251ba62ee7df21d45c2d11829930db
MarkDiamant <markdiamant@gmail.com>
2026-09-01 01:47:28 +0100

    Build first Luton Circumcision website draft

---
+++ app/layout.js
@@ -0,0 +1,5 @@
+import './globals.css';
+
+export const metadata={title:'Luton Circumcision | Experienced Circumcision Practitioner in Luton',description:'Professional circumcision service in Luton with over 20 years of experience, careful aftercare and a convenient local location.'};
+
+export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
